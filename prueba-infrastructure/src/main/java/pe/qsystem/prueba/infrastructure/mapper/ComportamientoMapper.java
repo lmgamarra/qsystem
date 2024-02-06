20f9e874389e372fcf12f40524e91638d57f1da2
lmgamarra <lm.gamarrach@gmail.com>
2024-02-06 11:41:58 -0500

    Ajustes en el openapi y los mappers

--- prueba-infrastructure/src/main/java/pe/qsystem/prueba/infrastructure/mapper/ComportamientoMapper.java
+++ prueba-infrastructure/src/main/java/pe/qsystem/prueba/infrastructure/mapper/ComportamientoMapper.java
@@ -1,12 +1,13 @@
 package pe.qsystem.prueba.infrastructure.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import pe.qsystem.prueba.domain.entity.ComportamientoDomEntity;
 import pe.qsystem.prueba.infrastructure.qsystemdb.entity.ComportamientoInfEntity;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ComportamientoMapper {
 
 	ComportamientoDomEntity asComportamientoPerCenterEntity(ComportamientoInfEntity personaInfEntity);
--- prueba-infrastructure/src/main/java/pe/qsystem/prueba/infrastructure/mapper/PersonaMapper.java
+++ prueba-infrastructure/src/main/java/pe/qsystem/prueba/infrastructure/mapper/PersonaMapper.java
@@ -3,14 +3,15 @@ package pe.qsystem.prueba.infrastructure.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import pe.qsystem.prueba.domain.entity.PersonaDomEntity;
 import pe.qsystem.prueba.infrastructure.qsystemdb.entity.PersonaInfEntity;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PersonaMapper {
-
+	
 	PersonaDomEntity asPersonaEntity(PersonaInfEntity personaInfEntity);
 	List<PersonaDomEntity> asPersonaEntitys(List<PersonaInfEntity> personaInfEntitys);
 	
